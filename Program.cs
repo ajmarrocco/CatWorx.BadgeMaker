ccdbdbdaf64824f4aa229a402c2fe8583d9a987a
ajmarrocco <ajmarrocco@gmail.com>
2022-06-08 19:25:27 -0400

    bool

--- Program.cs
+++ Program.cs
@@ -12,10 +12,13 @@ namespace CatWorx.BadgeMaker
             // Console.WriteLine($"greeting {greeting}"); 
             // Console.WriteLine("greeting: {0}", greeting, greetin);
             // How do you find the area of a square? Area = side * side
-            double side = 3.14;
-            double area = side * side;
-            Console.WriteLine("area: {0}", area);
-            Console.WriteLine("area is a {0}", area.GetType());
+            // double side = 3.14;
+            // double area = side * side;
+            // Console.WriteLine("area: {0}", area);
+            // Console.WriteLine("area is a {0}", area.GetType());
+            bool isCold = true;
+            Console.WriteLine(isCold ? "drink" : "add ice");  // output: drink
+            Console.WriteLine(!isCold ? "drink" : "add ice");  // output: add ice
         }
     }
 }
