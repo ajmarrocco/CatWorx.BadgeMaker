26083b011f668b3106674d616e87bb5328638ec8
ajmarrocco <ajmarrocco@gmail.com>
2022-06-08 19:21:55 -0400

    area

--- Program.cs
+++ Program.cs
@@ -6,11 +6,16 @@ namespace CatWorx.BadgeMaker
     {
         static void Main(string[] args)
         {
-            string greeting = "Hello";
-            string greetin = greeting + " World!";
-            // Console.WriteLine("greeting " + greeting);
-            Console.WriteLine($"greeting {greeting}"); 
-            Console.WriteLine("greeting: {0}", greeting, greetin);
+            // string greeting = "Hello";
+            // string greetin = greeting + " World!";
+            // // Console.WriteLine("greeting " + greeting);
+            // Console.WriteLine($"greeting {greeting}"); 
+            // Console.WriteLine("greeting: {0}", greeting, greetin);
+            // How do you find the area of a square? Area = side * side
+            double side = 3.14;
+            double area = side * side;
+            Console.WriteLine("area: {0}", area);
+            Console.WriteLine("area is a {0}", area.GetType());
         }
     }
 }
