dc16cc7756c1dbd6abe633ef06f28aa3def2a385
ajmarrocco <ajmarrocco@gmail.com>
2022-06-08 19:15:29 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CatWorx.BadgeMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "Hello";
+            string greetin = greeting + " World!";
+            // Console.WriteLine("greeting " + greeting);
+            Console.WriteLine($"greeting {greeting}"); 
+            Console.WriteLine("greeting: {0}", greeting, greetin);
+        }
+    }
+}
