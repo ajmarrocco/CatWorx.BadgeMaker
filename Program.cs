5cf53e5e7b6e441581519314518be27a44d55304
ajmarrocco <ajmarrocco@gmail.com>
2022-06-08 19:59:29 -0400

    dictionary

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CatWorx.BadgeMaker
 {
@@ -6,26 +7,32 @@ namespace CatWorx.BadgeMaker
     {
         static void Main(string[] args)
         {
-            // string greeting = "Hello";
-            // string greetin = greeting + " World!";
-            // // Console.WriteLine("greeting " + greeting);
-            // Console.WriteLine($"greeting {greeting}"); 
-            // Console.WriteLine("greeting: {0}", greeting, greetin);
-            // How do you find the area of a square? Area = side * side
-            // double side = 3.14;
-            // double area = side * side;
-            // Console.WriteLine("area: {0}", area);
-            // Console.WriteLine("area is a {0}", area.GetType());
-            // bool isCold = true;
-            // Console.WriteLine(isCold ? "drink" : "add ice");  // output: drink
-            // Console.WriteLine(!isCold ? "drink" : "add ice");  // output: add ice
-            string stringNum = "2";
-            Console.WriteLine(stringNum);
-            Console.WriteLine(stringNum.GetType());
-            // Convert to int
-            int intNum = Convert.ToInt16(stringNum);
-            Console.WriteLine(intNum);
-            Console.WriteLine(intNum.GetType());
+            // Declaring variable myScoreboard, a dictionary has a string key and an integer value
+            // Dictionary<string, int> myScoreBoard = new Dictionary<string, int>();
+            // // Adds multiple lines to the data
+            // myScoreBoard.Add("firstInning", 10);
+            // myScoreBoard.Add("secondInning", 20);
+            // myScoreBoard.Add("thirdInning", 30);
+            // myScoreBoard.Add("fourthInning", 40);
+            // myScoreBoard.Add("fifthInning", 50);
+            // Equivalent of top
+            Dictionary<string, int> myScoreBoard = new Dictionary<string, int>(){
+                { "firstInning", 10 },
+                { "secondInning", 20},
+                { "thirdInning", 30},
+                { "fourthInning", 40},
+                { "fifthInning", 50}
+            };
+            Console.WriteLine("----------------");
+            Console.WriteLine("  Scoreboard");
+            Console.WriteLine("----------------");
+            Console.WriteLine("Inning |  Score");
+            Console.WriteLine("   1   |    {0}", myScoreBoard["firstInning"]);
+            Console.WriteLine("   2   |    {0}", myScoreBoard["secondInning"]);
+            Console.WriteLine("   3   |    {0}", myScoreBoard["thirdInning"]);
+            Console.WriteLine("   4   |    {0}", myScoreBoard["fourthInning"]);
+            Console.WriteLine("   5   |    {0}", myScoreBoard["fifthInning"]);
+            Console.WriteLine($"score is {myScoreBoard["firstInning"]}");
         }
     }
 }
