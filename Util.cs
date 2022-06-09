7cda07f51d5a24e9392bb86bf43cc8e99ec426e6
ajmarrocco <ajmarrocco@gmail.com>
2022-06-09 18:39:59 -0400

    set up Util to make a data folder

--- Util.cs
+++ Util.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Collections.Generic;
 
 namespace CatWorx.BadgeMaker
@@ -16,7 +17,11 @@ namespace CatWorx.BadgeMaker
         }
         public static void MakeCSV(List<Employee> employees)
         {
-            
+            if(!Directory.Exists("data"))
+            {
+                // If not, create it
+                Directory.CreateDirectory("data");
+            }
         }
     }
 }
