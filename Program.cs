29bd496afe5979267f74986ae009e2e22aa46125
ajmarrocco <ajmarrocco@gmail.com>
2022-06-09 10:11:42 -0400

    ReadEmployees method

--- Program.cs
+++ Program.cs
@@ -5,31 +5,38 @@ namespace CatWorx.BadgeMaker
 {
     class Program
     {
+        static List<string> GetEmployees()
+        {
+            List<string> employees = new List<string>();
+            while (true)
+            {
+
+                Console.WriteLine("Please enter a name: (leave empty to exit): ");
+
+                string input = Console.ReadLine();
+
+                if (input == "")
+                {
+                    break;
+                }
+                employees.Add(input);
+            }
+            return employees;
+        }
+
+        static void PrintEmployees(List<string> employees)
+        {
+            for (int i = 0; i < employees.Count; i++)
+            {
+                Console.WriteLine(employees[i]);
+            }
+        }
+
         static void Main(string[] args)
         {
-            // Gets employees
-            List <string> employees = GetEmployees();
-            for (int i = 0; i < employees.Count; i++) 
-                {
-                    Console.WriteLine(employees[i]);
-                }
+            List<string> employees = GetEmployees();
+            PrintEmployees(employees);
         }
-        static List<string> GetEmployees()
-            {
-                List<string> employees = new List<string>();
-                    while(true)
-                    {
-                        Console.WriteLine("Please enter a name: ");
-                        // Get a name from the console and assign it to a variable
-                        string input = Console.ReadLine();
-                        // breaks the while loop
-                        if(input == "")
-                        {
-                            break;
-                        }
-                        employees.Add(input);                        
-                    }
-                    return employees;
-            }
+        
     }
 }
