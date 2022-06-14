97c4925da8b403deec6624b4222cf402aba7df2d
ajmarrocco <ajmarrocco@gmail.com>
2022-06-14 14:52:14 -0400

    move GetEmployees() to PeopleFetcher class

--- PeopleFetcher.cs
+++ PeopleFetcher.cs
@@ -2,9 +2,35 @@ using System;
 using System.Collections.Generic;
 
 namespace CatWorx.BadgeMaker {
+
     class PeopleFetcher
     {
         // code from GetEmployees() in Program.cs
-        
+        public static List<Employee> GetEmployees()
+        {
+            List<Employee> employees = new List<Employee>();
+            while (true)
+            {
+
+                Console.WriteLine("Please enter a name: (leave empty to exit): ");
+
+                string firstName = Console.ReadLine();
+
+                if (firstName == "")
+                {
+                    break;
+                }
+                // add a Console.ReadLine() for each value
+                Console.Write("Enter last name: ");
+                string lastName = Console.ReadLine();
+                Console.Write("Enter ID: ");
+                int id = Int32.Parse(Console.ReadLine());
+                Console.Write("Enter Photo URL: ");
+                string photoUrl = Console.ReadLine();
+                Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
+                employees.Add(currentEmployee);
+                }
+                return employees;
+        }
     }
 }
